e44e923aff54ebc3f3bd420796ba0baae0591d4f
Rachada Chairangsaris <rachada_32@hotmail.com>
2018-05-14 15:29:59 -0400

    created actionCreator type: FETCH_WEATHER and use axios to get the information from API.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,17 @@
+import axios from 'axios';
+
 const API_KEY = '07f832fdd08f39602887402a61d617a7';
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?&appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+
+  const url =`${ROOT_URL}&q=${city},us`
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
