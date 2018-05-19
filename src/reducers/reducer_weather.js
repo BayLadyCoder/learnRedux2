26177db45b0febd77f0f55734c2665b883983f3b
Rachada Chairangsaris <rachada_32@hotmail.com>
2018-05-19 16:31:57 -0400

    added FETCH_WEATHER action inreducer_weather.js that fetch city and return a collection of each searched city in an array.

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,4 +1,11 @@
-export default function (state = null, action) {
-  console.log('Action', action);
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function (state = [], action) {
+  switch (action.type) {
+
+    case FETCH_WEATHER:
+    return [action.payload.data, ...state];
+    // it will be like [city, city, city] NOT [city, [city, city]]
+  }
   return state;
 }
