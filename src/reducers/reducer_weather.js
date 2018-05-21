b1f4be03f26291342b7c8982c9bf5b3b792dcf55
Rachada Chairangsaris <rachada_32@hotmail.com>
2018-05-21 18:41:19 -0400

    added some comments.

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -5,6 +5,7 @@ export default function (state = [], action) {
 
     case FETCH_WEATHER:
     return !action.payload.data ? state : [action.payload.data, ...state];
+    // same as state.concat([action.payload.data])
     // it will be like [city, city, city] NOT [city, [city, city]]
   }
   return state;
